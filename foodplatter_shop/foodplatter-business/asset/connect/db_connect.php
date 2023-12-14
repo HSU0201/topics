62a21816c7e92d9c9755eaf5bb24f2f2ff19fe20
Shang Min <happymin0318@gmail.com>
2023-12-14 20:18:59 +0800

    整合2版

--- foodplatter_shop/foodplatter-business/asset/connect/db_connect.php
+++ foodplatter_shop/foodplatter-business/asset/connect/db_connect.php
@@ -10,5 +10,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("連線失敗: " . $conn->connect_error);
 } else {
-    echo "資料庫連線成功";
+    // echo "資料庫連線成功";
 }
